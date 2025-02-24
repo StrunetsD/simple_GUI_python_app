b504e2953f56a18d83c620154aea990ac25f6bba
Dmitry Strunets <d.strunetss@gmail.com>
2025-02-25 02:06:32 +0300

    [LAB-2 CREATE FACTORY.py] Add factory file to filling db

---
+++ database/models_factory.py
@@ -0,0 +1,40 @@
+import factory
+from factory.alchemy import SQLAlchemyModelFactory
+
+from db_conn import get_session
+from models import Parent, Student
+
+
+class ParentFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Parent
+        sqlalchemy_session = None
+        sqlalchemy_session_persistence = "commit"
+
+    first_name = factory.Faker('first_name', locale='ru_RU')
+    last_name = factory.Faker('last_name', locale='ru_RU')
+    middle_name = factory.Faker('first_name', locale='ru_RU')
+    income = factory.Faker('pydecimal', left_digits=5, right_digits=2, positive=True)
+    gender = factory.Iterator(['male', 'female'])
+
+
+class StudentFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Student
+        sqlalchemy_session = None
+        sqlalchemy_session_persistence = "commit"
+
+    first_name = factory.Faker('first_name', locale='ru_RU')
+    last_name = factory.Faker('last_name', locale='ru_RU')
+    middle_name = factory.Faker('first_name', locale='ru_RU')
+    father = factory.SubFactory(ParentFactory)
+    mother = factory.SubFactory(ParentFactory)
+    brothers_count = factory.Faker('random_int', min=0, max=5)
+    sisters_count = factory.Faker('random_int', min=0, max=5)
+
+
+if __name__ == "__main__":
+    with get_session() as session:
+        ParentFactory._meta.sqlalchemy_session = session
+        StudentFactory._meta.sqlalchemy_session = session
+        students = [StudentFactory() for _ in range(10)]
