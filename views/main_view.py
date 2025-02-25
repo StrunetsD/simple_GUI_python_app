4f2952a9bd1c3441cd6a5c6881514c062f59bbc7
Dmitry Strunets <d.strunetss@gmail.com>
2025-02-25 23:51:20 +0300

    [LAB-1 MAIN_VIEW] Add other modules into  main_view

--- views/main_view.py
+++ views/main_view.py
@@ -1,31 +1,189 @@
-import tkinter as tk
-from tree_view import TableView
-from model.db_requests import DBRequests
+from views.table_view import TableView
+from dialog import *
+from views.pagination import Pagination
+from controllers.controllers import Controller
 
 
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Table View Example")
+        self.title("Студенты")
         self.geometry("600x400")
-        db = DBRequests()
-
-        columns = (
-                    "ФИО студента",
-                    "ФИО Отца",
-                    "заработок отца",
-                    "ФИО Матери",
-                    "заработок матери",
-                    "число братьев",
-                    "число сестер"
-                    )
-
-        self.table_view = TableView(self, columns)
+        self.controller = Controller()
+        self.table_view = TableView(self)
         self.table_view.pack(fill=tk.BOTH, expand=True)
 
-        data = db.get_query()
+        self.data = self.controller.get_students()
+        self.pagination = Pagination(len(self.data))
 
-        self.table_view.insert_data(data)
+        controls = self.create_controls(self, self.pagination, self.update_table)
+        self.status_label = controls["status_label"]
+
+        self.update_table()
+        self.update_status_label(self.pagination, self.status_label)
+
+        self.create_menu()
+
+    def load_data(self):
+        self.data = self.controller.get_students()
+        self.pagination.update_total(len(self.data))
+
+        if self.pagination.current_page > self.pagination.total_pages:
+            self.pagination.current_page = self.pagination.total_pages
+        elif self.pagination.current_page < 1:
+            self.pagination.current_page = 1
+
+        self.update_table()
+        self.update_status_label(self.pagination, self.status_label)
+
+    def update_table(self):
+        current_data = self.pagination.get_current_page_data(self.data)
+        self.table_view.clear_data()
+        self.table_view.insert_data(current_data)
+
+    def previous_page(self):
+        self.pagination.previous_page()
+        self.update_table()
+        self.update_status_label(self.pagination, self.status_label)
+
+    def next_page(self):
+        self.pagination.next_page()
+        self.update_table()
+        self.update_status_label(self.pagination, self.status_label)
+
+    def count(self):
+        deleted_count, found_count = self.controller.get_counts()
+        messagebox.showinfo("Статистика", f"Удалено записей: {deleted_count}\nНайдено записей: {found_count}")
+
+    def create_menu(self):
+        menubar = tk.Menu(self)
+        self.config(menu=menubar)
+
+        file_menu = tk.Menu(menubar, tearoff=0)
+        file_menu.add_command(label="Поиск студента по имени", command=self.open_search_student_dialog)
+        file_menu.add_command(label="Поиск родителя по имени", command=self.open_search_parent_dialog)
+        file_menu.add_command(label="Поиск по братьям/сестрам", command=self.open_siblings_search_dialog)
+        file_menu.add_command(label="Поиск по доходу", command=self.open_income_search_dialog)
+        file_menu.add_separator()
+        file_menu.add_command(label="Удалить студента по имени", command=self.open_delete_student_dialog)
+        file_menu.add_command(label="Удалить по братьям/сестрам", command=self.open_delete_siblings_dialog)
+        file_menu.add_command(label="Удалить по доходу", command=self.open_delete_income_dialog)
+        file_menu.add_command(label="Обновить данные", command=self.load_data)
+        file_menu.add_command(label="Статистика", command=self.count)
+        menubar.add_cascade(label="Операции", menu=file_menu)
+
+    def open_search_student_dialog(self):
+        try:
+            dialog = SearchStudentByNameDialog(self, self.controller)
+            self.wait_window(dialog)
+            self.load_data()
+        except Exception as e:
+            print(f"Ошибка при открытии диалога поиска студента: {e}")
+
+    def open_search_parent_dialog(self):
+        dialog = SearchParentByNameDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+
+    def open_siblings_search_dialog(self):
+        dialog = SearchBySiblingsDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+
+    def open_income_search_dialog(self):
+        dialog = IncomeSearchDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+
+    def open_delete_student_dialog(self):
+        dialog = DeleteStudentByNameDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+        deleted, found = self.controller.get_counts()
+        messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
+
+    def open_delete_siblings_dialog(self):
+        dialog = DeleteBySiblingsDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+        deleted, found = self.controller.get_counts()
+        messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
+
+    def open_delete_income_dialog(self):
+        dialog = DeleteByIncomeDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+        deleted, found = self.controller.get_counts()
+        messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
+
+    @staticmethod
+    def update_status_label(pagination, status_label):
+        status_label.config(
+            text=f"Страница {pagination.current_page} из {pagination.total_pages} (Размер страницы: {pagination.page_size})")
+
+    def create_controls(self, parent, pagination, update_table_callback):
+        control_frame = tk.Frame(parent)
+        control_frame.pack(fill=tk.X, pady=10, padx=10)
+
+        nav_frame = tk.Frame(control_frame)
+        nav_frame.pack(side=tk.LEFT)
+
+        first_button = tk.Button(nav_frame, text="<<",
+                                 command=lambda: [
+                                     pagination.first_page() if pagination else None,
+                                     update_table_callback(),
+                                     self.update_status_label(pagination, status_label) if pagination else None])
+
+        first_button.pack(side=tk.LEFT)
+
+        prev_button = tk.Button(nav_frame, text="<",
+                                command=lambda: [
+                                    pagination.previous_page() if pagination else None,
+                                    update_table_callback(),
+                                    self.update_status_label(pagination, status_label) if pagination else None])
+
+        prev_button.pack(side=tk.LEFT, padx=5)
+
+        next_button = tk.Button(nav_frame, text=">", command=lambda: [
+            pagination.next_page() if pagination else None,
+            update_table_callback(),
+            self.update_status_label(pagination, status_label) if pagination else None])
+
+        next_button.pack(side=tk.LEFT)
+
+        last_button = tk.Button(nav_frame, text=">>", command=lambda: [
+            pagination.last_page() if pagination else None,
+            update_table_callback(),
+            self.update_status_label(pagination, status_label) if pagination else None])
+
+        last_button.pack(side=tk.LEFT, padx=5)
+
+        page_size_var = tk.StringVar(value="10")
+        sizes = ("10", "20", "50", "100")
+        page_size_menu = tk.OptionMenu(
+            control_frame,
+            page_size_var,
+            *sizes,
+            command=lambda value: [
+                pagination.set_page_size(int(value)) if pagination else None,
+                update_table_callback(),
+                self.update_status_label(pagination, status_label) if pagination else None])
+
+        page_size_menu.pack(side=tk.RIGHT)
+
+        info_frame = tk.Frame(control_frame)
+        info_frame.pack(side=tk.RIGHT, padx=20)
+
+        status_label = tk.Label(info_frame, text="")
+        status_label.pack()
+
+        if pagination:
+            self.update_status_label(pagination, status_label)
+
+        return {
+            "status_label": status_label,
+            "page_size_var": page_size_var,
+        }
 
 
 if __name__ == "__main__":
