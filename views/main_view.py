ba18b1ffacfc24676086fb9e11e330642627f88f
Dmitry Strunets <d.strunetss@gmail.com>
2025-03-02 11:34:27 +0300

    [LAB-2 ADD POSSIBILITY OF WORKING WITH XML] Made it possible to select the operating mode in main window

--- views/main_view.py
+++ views/main_view.py
@@ -1,22 +1,46 @@
 from views.table_view import TableView
-from dialog import *
+from dialog_view import *
 from views.pagination import Pagination
 from controllers.controllers import Controller
+from tkinter import ttk, messagebox
+
+
+class StartWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Выбор режима работы")
+        self.geometry("600x400")
+
+        ttk.Label(self, text="Выберите источник данных:").pack(pady=10)
+
+        ttk.Button(self, text="База данных",
+                   command=lambda: self.start_app("db")).pack(pady=5)
+
+        ttk.Button(self, text="XML файлы",
+                   command=lambda: self.start_app("xml")).pack(pady=5)
+
+    def start_app(self, mode):
+        self.destroy()
+        app = MainWindow(mode)
+        app.mainloop()
 
 
 class MainWindow(tk.Tk):
-    def __init__(self):
+    def __init__(self, mode):
         super().__init__()
         self.title("Студенты")
         self.geometry("600x400")
-        self.controller = Controller()
+        self.mode = mode
+        self.controller = Controller(mode)
+        ttk.Label(self, text=f"Режим работы: {mode.upper()}").pack(side=tk.TOP, pady=5)
+
         self.table_view = TableView(self)
         self.table_view.pack(fill=tk.BOTH, expand=True)
 
         try:
             self.data = self.controller.get_students()
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Не удалось загрузить данные студентов: {e}")
+        except Exception:
+            messagebox.showerror("Ошибка", f"Не удалось загрузить данные студентов")
             self.data = []
 
         self.pagination = Pagination(len(self.data))
@@ -41,39 +65,39 @@ class MainWindow(tk.Tk):
 
             self.update_table()
             self.update_status_label(self.pagination, self.status_label)
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Не удалось обновить данные: {e}")
+        except Exception:
+            messagebox.showerror("Ошибка", f"Не удалось обновить данные")
 
     def update_table(self):
         try:
             current_data = self.pagination.get_current_page_data(self.data)
             self.table_view.clear_data()
             self.table_view.insert_data(current_data)
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Не удалось обновить таблицу: {e}")
+        except Exception:
+            messagebox.showerror("Ошибка", f"Не удалось обновить таблицу")
 
     def previous_page(self):
         try:
             self.pagination.previous_page()
             self.update_table()
             self.update_status_label(self.pagination, self.status_label)
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Не удалось перейти на предыдущую страницу: {e}")
+        except Exception:
+            messagebox.showerror("Ошибка", f"Не удалось перейти на предыдущую страницу")
 
     def next_page(self):
         try:
             self.pagination.next_page()
             self.update_table()
             self.update_status_label(self.pagination, self.status_label)
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Не удалось перейти на следующую страницу: {e}")
+        except Exception:
+            messagebox.showerror("Ошибка", f"Не удалось перейти на следующую страницу")
 
     def count(self):
         try:
             deleted_count, found_count = self.controller.get_counts()
             messagebox.showinfo("Статистика", f"Удалено записей: {deleted_count}\nНайдено записей: {found_count}")
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Не удалось получить статистику: {e}")
+        except Exception:
+            messagebox.showerror("Ошибка", f"Не удалось получить статистику")
 
     def create_menu(self):
         menubar = tk.Menu(self)
@@ -94,74 +118,51 @@ class MainWindow(tk.Tk):
         menubar.add_cascade(label="Операции", menu=file_menu)
 
     def open_add_student_dialog(self):
-        try:
-            dialog = AddStudentDialog(self, self.controller)
-            self.wait_window(dialog.top)
-            self.load_data()
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при добавлении студента: {str(e)}")
+        dialog = AddStudentDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+
 
     def open_search_student_dialog(self):
-        try:
-            dialog = SearchStudentByNameDialog(self, self.controller)
-            self.wait_window(dialog)
-            self.load_data()
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при открытии диалога поиска студента: {e}")
+        dialog = SearchStudentByNameDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
 
     def open_search_parent_dialog(self):
-        try:
-            dialog = SearchParentByNameDialog(self, self.controller)
-            self.wait_window(dialog)
-            self.load_data()
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при открытии диалога поиска родителя: {e}")
+        dialog = SearchParentByNameDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
 
     def open_siblings_search_dialog(self):
-        try:
-            dialog = SearchBySiblingsDialog(self, self.controller)
-            self.wait_window(dialog)
-            self.load_data()
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при открытии диалога поиска брата/сестры: {e}")
+        dialog = SearchBySiblingsDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
 
     def open_income_search_dialog(self):
-        try:
-            dialog = IncomeSearchDialog(self, self.controller)
-            self.wait_window(dialog)
-            self.load_data()
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при открытии диалога поиска по заработку: {e}")
+        dialog = IncomeSearchDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
 
     def open_delete_student_dialog(self):
-        try:
-            dialog = DeleteStudentByNameDialog(self, self.controller)
-            self.wait_window(dialog)
-            self.load_data()
-            deleted, found = self.controller.get_counts()
-            messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при открытии диалога удаления студента: {e}")
+        dialog = DeleteStudentByNameDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+        deleted, found = self.controller.get_counts()
+        messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
 
     def open_delete_siblings_dialog(self):
-        try:
-            dialog = DeleteBySiblingsDialog(self, self.controller)
-            self.wait_window(dialog)
-            self.load_data()
-            deleted, found = self.controller.get_counts()
-            messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при открытии диалога удаления по брату/сестре: {e}")
+        dialog = DeleteBySiblingsDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+        deleted, found = self.controller.get_counts()
+        messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
 
     def open_delete_income_dialog(self):
-        try:
-            dialog = DeleteByIncomeDialog(self, self.controller)
-            self.wait_window(dialog)
-            self.load_data()
-            deleted, found = self.controller.get_counts()
-            messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
-        except Exception as e:
-            messagebox.showerror("Ошибка", f"Ошибка при открытии диалога удаления по доходу: {e}")
+        dialog = DeleteByIncomeDialog(self, self.controller)
+        self.wait_window(dialog)
+        self.load_data()
+        deleted, found = self.controller.get_counts()
+        messagebox.showinfo("Статистика", f"Всего удалено: {deleted}\nВсего найдено: {found}")
 
     @staticmethod
     def update_status_label(pagination, status_label):
@@ -175,52 +176,49 @@ class MainWindow(tk.Tk):
         nav_frame = tk.Frame(control_frame)
         nav_frame.pack(side=tk.LEFT)
 
-        first_button = tk.Button(nav_frame, text="<<",
-                                 command=lambda: [
-                                     pagination.first_page() if pagination else None,
-                                     update_table_callback(),
-                                     self.update_status_label(pagination, status_label) if pagination else None])
-
+        first_button = tk.Button(nav_frame, text="<<", command=lambda: self._handle_pagination_action(
+            lambda: pagination.first_page(), pagination, update_table_callback, status_label
+        ))
         first_button.pack(side=tk.LEFT)
 
-        prev_button = tk.Button(nav_frame, text="<",
-                                command=lambda: [
-                                    pagination.previous_page() if pagination else None,
-                                    update_table_callback(),
-                                    self.update_status_label(pagination, status_label) if pagination else None])
-
+        prev_button = tk.Button(nav_frame, text="<", command=lambda: self._handle_pagination_action(
+            lambda: pagination.previous_page(), pagination, update_table_callback, status_label
+        ))
         prev_button.pack(side=tk.LEFT, padx=5)
 
-        next_button = tk.Button(nav_frame, text=">", command=lambda: [
-            pagination.next_page() if pagination else None,
-            update_table_callback(),
-            self.update_status_label(pagination, status_label) if pagination else None])
-
+        next_button = tk.Button(nav_frame, text=">", command=lambda: self._handle_pagination_action(
+            lambda: pagination.next_page(), pagination, update_table_callback, status_label
+        ))
         next_button.pack(side=tk.LEFT)
 
-        last_button = tk.Button(nav_frame, text=">>", command=lambda: [
-            pagination.last_page() if pagination else None,
-            update_table_callback(),
-            self.update_status_label(pagination, status_label) if pagination else None])
-
+        last_button = tk.Button(nav_frame, text=">>", command=lambda: self._handle_pagination_action(
+            lambda: pagination.last_page(), pagination, update_table_callback, status_label
+        ))
         last_button.pack(side=tk.LEFT, padx=5)
 
-        page_size_var = tk.StringVar(value="10")
-        sizes = ("10", "20", "50", "100")
-        page_size_menu = tk.OptionMenu(
-            control_frame,
-            page_size_var,
-            *sizes,
-            command=lambda value: [
-                pagination.set_page_size(int(value)) if pagination else None,
-                update_table_callback(),
-                self.update_status_label(pagination, status_label) if pagination else None])
+        page_size_frame = tk.Frame(control_frame)
+        page_size_frame.pack(side=tk.RIGHT, padx=10)
 
-        page_size_menu.pack(side=tk.RIGHT)
+        tk.Label(page_size_frame, text="Строк на странице:").pack(side=tk.LEFT)
+
+        page_size_var = tk.StringVar(value="10")
+
+        validation = (parent.register(self._validate_page_size), '%P')
+        page_size_entry = tk.Entry(
+            page_size_frame,
+            textvariable=page_size_var,
+            validate="key",
+            validatecommand=validation,
+            width=5
+        )
+        page_size_entry.pack(side=tk.LEFT, padx=5)
+
+        page_size_entry.bind("<Return>", lambda e: self._apply_page_size(
+            page_size_var, pagination, update_table_callback, status_label
+        ))
 
         info_frame = tk.Frame(control_frame)
         info_frame.pack(side=tk.RIGHT, padx=20)
-
         status_label = tk.Label(info_frame, text="")
         status_label.pack()
 
@@ -232,7 +230,25 @@ class MainWindow(tk.Tk):
             "page_size_var": page_size_var,
         }
 
+    @staticmethod
+    def _validate_page_size(new_value):
+        return new_value.isdigit() or new_value == ""
 
-if __name__ == "__main__":
-    app = MainWindow()
-    app.mainloop()
+    def _apply_page_size(self, page_size_var, pagination, update_callback, status_label):
+        try:
+            new_size = int(page_size_var.get())
+            if new_size <= 0:
+                raise ValueError
+            if pagination:
+                pagination.set_page_size(new_size)
+                update_callback()
+                self.update_status_label(pagination, status_label)
+        except ValueError:
+            messagebox.showerror("Ошибка", "Введите положительное число")
+            page_size_var.set(str(pagination.page_size if pagination else 10))
+
+    def _handle_pagination_action(self, action, pagination, update_callback, status_label):
+        if pagination:
+            action()
+            update_callback()
+            self.update_status_label(pagination, status_label)
